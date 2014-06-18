f9f4b75365493ecbe5e470788cff8a799b5b8ba3
Roman Novitsky <roman.novitsky@outlook.com>
2014-06-18 11:57:00 +0400

    Added Gemini package.

---
+++ MottoBeneApps.GoHome/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MottoBeneApps.GoHome
+{
+    #region Namespace Imports
+
+    using System.Windows;
+
+    #endregion
+
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
